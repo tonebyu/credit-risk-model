1571932bb40a2011feeff97a100ff4089e898a0b
nebyu <tonebyu@gmail.com>
2025-07-01 16:27:48 +0300

    Task 3: Feature engineering pipeline for aggregation, datetime, scaling, encoding

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ssss\Desktop\## 10 Acadamy\Week5\Credit-Scoring-and-Predictive-Risk-Modeling\venv
+set VIRTUAL_ENV=C:\Users\ssss\Desktop\## 10 Acadamy\Week5\credit-risk-model\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
